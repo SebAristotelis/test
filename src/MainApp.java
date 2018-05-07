35d0aff47941cdb40bee7c3863c476e0d0958dd8
SebAristotelis <38750403+SebAristotelis@users.noreply.github.com>
2018-05-07 18:55:35 +1000

    Initial Commit

---
+++ src/MainApp.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+
+public class MainApp extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("MainApp", args);
+    }
+
+    public void settings() {
+        size(800, 800);
+    }
+
+    public void setup() {
+        int n = 0;
+    }
+    public void draw(){
+
+    }
+
+}
